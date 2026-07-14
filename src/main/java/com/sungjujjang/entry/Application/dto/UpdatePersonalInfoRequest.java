b5df179e2b99451f8df83b9dea881667ac27351e
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-07-15 08:22:00 +0900

    fix: fix함

--- src/main/java/com/sungjujjang/entry/Application/dto/UpdatePersonalInfoRequest.java
+++ src/main/java/com/sungjujjang/entry/Application/dto/UpdatePersonalInfoRequest.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Pattern;
 public record UpdatePersonalInfoRequest(
         String name,
         @Pattern(
-                regexp = "^(?:\d{2})(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])$",
+                regexp = "^(?:d{2})(?:0[1-9]|1[0-2])(?:0[1-9]|[12]d|3[01])$",
                 message = "생년월일은 YYMMDD 형태로 입력하여 주세요."
         )
         String birth,
